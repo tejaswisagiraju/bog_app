51aa20d4bc9cb300079d2064c9047249fb3b3950
Tejaswi Sagiraju <tejaswi.sagiraju@gmail.com>
2015-06-08 09:16:53 -0700

    making first push with CRUD elements

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,47 @@
+class CreaturesController < ApplicationController 
+
+	def index
+		@creatures = Creature.all 
+		render :index
+	end
+
+	def new
+		@creature = Creature.new
+		render :new
+	end
+
+	def create
+		new_creature = params.require(:creature).permit(:name,:description)
+		    creature = Creature.create(new_creature)
+            redirect_to "/creatures/#{creature.id}"
+	end
+
+	def show
+		id = params[:id]
+		@creature = Creature.find(id)
+		render :show
+	end
+
+	def edit
+		id = params[:id]
+		@creature = Creature.find(id)
+		render :edit
+	end
+
+	def update
+		creature_id = params[:id]
+		creature = Creature.find(creature_id)
+		updated_attributes = params.require(:creature).permit(:name , :description)
+		Creature.update_attributes(updated_attributes)
+		redirect_to "/creatures/#{creature_id}"
+	end
+
+	def destroy
+		id = params[:id]
+		creature = Creature.find(id)
+		creature.destroy
+		redirect_to "/creatures"
+
+	end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  #setting the root route
+  root to:'creatures#index'
+
+  get '/creatures', to: "creatures#index" , as: "creatures"
+
+  get '/creatures/new', to: "creatures#new", as: "new_creature"
+  get '/creatures/:id', to: "creatures#show", as: "creature"
+  get '/creatures/:id/edit', to:"creatures#show", as:"edit_creature"
+
+  post "/creatures", to: "creatures#create"
+
+  patch "/creatures/:id", to: "creatures#update"
+  delete "/creatures/:id", to: "creatures#destroy" 
+
+end
